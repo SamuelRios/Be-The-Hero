6906ad172ec909ff6afa454d8843ac67d0aa875f
Samuel Rios <mucarioss@gmail.com>
2020-03-28 01:23:28 -0300

    Adicionando Frontend

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -6,7 +6,7 @@ module.exports = {
         const index = await connection('ongs').select('*');
         return response.json(index);
     },
-    async create(request, reponse){
+    async create(request, response){
         const {name, email, whatsapp, city, uf} = request.body;
         const id = crypto.randomBytes(4).toString('HEX');
         await connection('ongs').insert({
